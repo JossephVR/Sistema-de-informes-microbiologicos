06489990395004ad357860b7a2aa672ec25eefdd
prosteam <jossephvr@gmail.com>
2026-04-26 15:30:12 -0600

    feat: implement dotenv for database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 // Configuración de la conexión a la base de datos PostgreSQL
-
 const pool = new Pool({
-  user: 'user', //agregar usuario       
-  host: 'host', // agregar host  
-  database: 'database', // agregar nombre de la base de datos
-  password: 'password', // agregar contraseña
-  port: 5432,                
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Exporta el pool de conexiones para que pueda ser utilizado en otros módulos (ej. apiRoutes.js)
